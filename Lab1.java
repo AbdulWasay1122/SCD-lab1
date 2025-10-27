176de70f8fb364b9b16dc689413cee63a6d2de99
AbdulWasay1122 <154363489+AbdulWasay1122@users.noreply.github.com>
2025-10-27 15:38:42 +0500

    Update output message in Lab1 class

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,6 @@
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Roll No:2023F-BSE-145 \nSCD Lab 01");
     }
 }
+
